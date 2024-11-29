14e7b6c975740272277a78d9f7d62f472fca9c86
Sơn Tùng Kiều <kieusontung8@gmail.com>
2024-11-29 20:48:49 +0700

    p4

--- Lab02/AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
@@ -1,66 +1,44 @@
 package hust.soict.dsai.aims.disc;
 
-public class DigitalVideoDisc {
+import hust.soict.dsai.aims.media.Media;
+
+public class DigitalVideoDisc extends Media{
 	private static int nbDigitalVideoDiscs=0;
-	private String title;
-	private String category;
 	private String director;
 	private int length;
-	private float cost;
-	public String getTitle() {
-		return title;
-	}
-	public String getCategory() {
-		return category;
-	}
 	public String getDirector() {
 		return director;
 	}
 	public int getLength() {
 		return length;
 	}
-	public float getCost() {
-		return cost;
-	}
 	public DigitalVideoDisc(String title) {
-		super();
+		super(nbDigitalVideoDiscs, title, "", 0); 
 		nbDigitalVideoDiscs+=1;
-		this.title = title;
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
-		super();
+		super(nbDigitalVideoDiscs,title,category,cost);
 		nbDigitalVideoDiscs+=1;
-
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
+		
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
-		super();
+		super(nbDigitalVideoDiscs,title,category,cost);
 		nbDigitalVideoDiscs+=1;
-
-		this.title = title;
-		this.category = category;
 		this.director = director;
-		this.cost = cost;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-		super();
+		super(nbDigitalVideoDiscs,title,category,cost);
 		nbDigitalVideoDiscs+=1;
-
-		this.title = title;
-		this.category = category;
 		this.director = director;
 		this.length = length;
-		this.cost = cost;
 	}
 	public void setTitle(String newTitle) {
 		// TODO Auto-generated method stub
-		title = newTitle;
+		super.setTitle(newTitle);
 		
 	}
     public String toString() {
-        return "DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + " $";
+        return "DVD - " + this.getTitle() + " - " + this.getCategory() + " - " + director + " - " + length + ": " + this.getCost() + " $";
     }
     
     
