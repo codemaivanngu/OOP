51d52ab544ce80b01f651c6484887612e5994d77
Sơn Tùng Kiều <kieusontung8@gmail.com>
2024-11-29 21:38:37 +0700

    b7

--- Lab02/AimsProject/src/hust/soict/dsai/aims/cart/Cart.java
+++ Lab02/AimsProject/src/hust/soict/dsai/aims/cart/Cart.java
@@ -1,55 +1,55 @@
 package hust.soict.dsai.aims.cart;
 
-import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Media;
 
 public class Cart {
 	public static final int MAX_NUMBERS_ORDERED = 20;
-	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private Media itemsOrdered[] = new Media[MAX_NUMBERS_ORDERED];
 	private int qtyOrdered = 0;
-	public String addDigitalVideoDisc(DigitalVideoDisc disc) {
+	public String addMedia(Media media) {
 		if (qtyOrdered < MAX_NUMBERS_ORDERED) {
-			itemsOrdered[qtyOrdered] = disc;
+			itemsOrdered[qtyOrdered] = media;
 			qtyOrdered += 1;
-			return "The disc has been added" ;
+			return "The media has been added" ;
 		}
 		return "The cart is almost full";
 	}
-	public String addDigitalVideoDisc(DigitalVideoDisc[] discs) {
-		if (qtyOrdered + discs.length <= MAX_NUMBERS_ORDERED) {
-			for(int i=0;i<discs.length;i++){
-				itemsOrdered[qtyOrdered] = discs[i];
+	public String addDigitalVideoDisc(Media[] medias) {
+		if (qtyOrdered + medias.length <= MAX_NUMBERS_ORDERED) {
+			for(int i=0;i<medias.length;i++){
+				itemsOrdered[qtyOrdered] = medias[i];
 				qtyOrdered += 1;
 			}
-			return "The disc has been added" ;
+			return "The media has been added" ;
 		}
 		return "The cart is almost full";
 	}
-	public String addDigitalVideoDisc(DigitalVideoDisc disc1, DigitalVideoDisc disc2) {
+	public String addDigitalVideoDisc(Media media1, Media media2) {
 		if (qtyOrdered +2 <= MAX_NUMBERS_ORDERED) {
-			itemsOrdered[qtyOrdered] = disc1;
+			itemsOrdered[qtyOrdered] = media1;
 			qtyOrdered += 1;
-			itemsOrdered[qtyOrdered] = disc2;
+			itemsOrdered[qtyOrdered] = media2;
 			qtyOrdered += 1;
-			return "The disc has been added" ;
+			return "The media has been added" ;
 		}
 		return "The cart is almost full";
 	}
 	
-	public String removeDigitalVideoDisc(DigitalVideoDisc disc) {
+	public String removeDigitalVideoDisc(Media media) {
 		int p=-1;
 		for (int i=0;i<qtyOrdered;i++) {
-			if (itemsOrdered[i]==disc) {
+			if (itemsOrdered[i]==media) {
 				p=i;
 			}
 		}
 		if (p==-1) {
-			return "The disc is not in the cart";
+			return "The media is not in the cart";
 		}
 		for(int i=p;i<qtyOrdered;i++) {
 			itemsOrdered[i]=itemsOrdered[i+1];
 		}
 		qtyOrdered -= 1;
-		return "The disc is removed";
+		return "The media has been removed";
 	}
 	public float totalCost() {
 		float res=0;
@@ -61,7 +61,6 @@ public class Cart {
         System.out.println("Ordered Items: ");
         float totalCost = 0;
         for (int i = 0; i < qtyOrdered; i++) {
-//            DigitalVideoDisc disc = itemsOrdered[i];
             System.out.println((i + 1) + ". " + itemsOrdered[i].toString());
             totalCost += itemsOrdered[i].getCost();
         }
